e6017fdce626c034bb6f9741eecd499dadbf8087
Ravi Garg <ravi97garg@gmail.com>
2020-12-05 13:15:28 +0530

    added blog page

---
+++ src/containers/ContactUsContainer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+class ContactUsContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                Contact Us
+            </div>
+        )
+    }
+}
+
+export default ContactUsContainer;
---
+++ src/containers/WisdomContainer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+class WisdomContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                Wisdom Batch 21
+            </div>
+        )
+    }
+}
+
+export default WisdomContainer;
